a939dceca93ed0f48cf358ecec742565df30f1a4
Sanjay Kumar <46316162+MrUnKnown786@users.noreply.github.com>
2021-02-23 13:39:54 +0530

    Add files via upload

---
+++ ASSIGNMENT 7 ( Dynamic Programming)/6.Min sum path in the matrix.cpp
@@ -0,0 +1,41 @@
+// Min sum path in the matrix
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define R 3
+#define C 3
+ 
+int min(int x, int y, int z);
+ 
+int minpath(int path[R][C], int m, int n)
+{
+    if (n < 0 || m < 0)
+        return INT_MAX;
+    else if (m == 0 && n == 0)
+        return cost[m][n];
+    else
+        return cost[m][n] + 
+         min(minCost(cost, m - 1, n - 1),
+             minCost(cost, m - 1, n),
+             minCost(cost, m, n - 1));
+}
+ 
+int min(int x, int y, int z)
+{
+    if (x < y)
+        return (x < z) ? x : z;
+    else
+        return (y < z) ? y : z;
+}
+ 
+int main()
+{
+    int path[R][C] = { { 1, 2, 3 }, 
+                       { 4, 8, 2 }, 
+                       { 1, 5, 3 } };
+                        
+    cout << minpath(cost, 2, 2) << endl;
+     
+    return 0;
+}
